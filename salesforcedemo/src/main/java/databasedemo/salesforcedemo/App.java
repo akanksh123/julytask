0d770ba2f1c449a6be43652200a7fb2b71b18103
akanksh123 <akankshbekal@gmail.com>
2019-07-07 14:10:55 +0530

    new

--- salesforcedemo/src/main/java/databasedemo/salesforcedemo/App.java
+++ salesforcedemo/src/main/java/databasedemo/salesforcedemo/App.java
@@ -8,62 +8,71 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Scanner;
 
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
 import org.apache.commons.csv.CSVRecord;
 
-/**
- * Hello world!
- *
- */
 public class App {
+	static String url = "jdbc:mariadb://localhost:3306/sales";
+	static String user = "tech";
+	static String password = "";
+	static String col, tabname;
+	static String sql;
+	static Scanner s = new Scanner(System.in);
+	static int n;
+	static ArrayList<String> columnname = new ArrayList<String>();
+	static String[] cname, ctype;
 
-	public static final String path = "C:/Users/LENOVO/Downloads/Account.csv";
+	public static void main(String[] args) throws SQLException {
+		tabname = getData();
+		System.out.println(tabname);
+		sql = "Select * from " + tabname;
+		Connection conn = DriverManager.getConnection(url, user, password);
 
-	public static void main(String[] args) {
-		String sql = "select * from Account";
+		PreparedStatement mystat = conn.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE,
+				ResultSet.CONCUR_UPDATABLE);
+		ResultSet rs = mystat.executeQuery();
+		ResultSetMetaData md = rs.getMetaData();
+		n = md.getColumnCount();
+		String[] column = new String[n + 1];
+		cname = new String[n + 1];
+		ctype = new String[n + 1];
+		checkRecords(md, mystat);
 
+		addData(conn, mystat, rs);
+
+	}
+
+	public static String getData() {
+		System.out.println("Enter the table name");
+		String name = s.nextLine();
+		return name;
+
+	}
+
+	public static void checkRecords(ResultSetMetaData md, PreparedStatement stat) {
 		try {
-
-			Connection con = DriverManager.getConnection("jdbc:mariadb://localhost:3306/sales?user=tech&password=");
-			PreparedStatement mystat = con.prepareStatement(sql, ResultSet.TYPE_SCROLL_INSENSITIVE,
-					ResultSet.CONCUR_UPDATABLE);
-			ResultSet rs = mystat.executeQuery();
-			ResultSetMetaData md = rs.getMetaData();
-			int n = md.getColumnCount();
-			String[] column = new String[n + 1];
-			String[] columnname = new String[n + 1];
-			File file = new File("C:/Users/LENOVO/Downloads/Account.csv");
-			FileReader in = new FileReader(file);
-			CSVParser parse = CSVParser.parse(file, Charset.forName("UTF-8"),
-					CSVFormat.RFC4180.withFirstRecordAsHeader());
-			Iterable<CSVRecord> records = parse.getRecords();
-			/*
-			 * BufferedReader br = new BufferedReader(new FileReader(path)); String header =
-			 * br.readLine(); if (header != null) { columnname = header.split(","); }
-			 */
-
-			for (CSVRecord record : records) {
-				rs.moveToInsertRow();
-				for (int i = 0; i < n - 1; i++) {
-					column[i] = record.get(i);
-					if (column[i].isEmpty()) {
-						continue;
-					}
-					columnname[i] = md.getColumnName(i + 1);
-					// System.out.println("Hello" + columnname[i]);
-					// System.out.println(column[i]);
-
-					rs.updateString(columnname[i], column[i]);
-
-				} /*
-					 * for(int i=1;i<66;i++) { column[i]=record.get(i); }
-					 */
-				rs.insertRow();
-				rs.moveToCurrentRow();
-				// ResultSet rs2=mystat.executeQuery("INSERT INTO Account (columnname)VALUES
-				// (column[])");
+			getRecords();
+			for (int i = 0, j = 0; i < n && j < n; i++) {
+				cname[i] = md.getColumnName(i + 1);
+				ctype[i] = md.getColumnTypeName(i + 1);
+				// System.out.println(cname[i]);
+				if (cname[i].equalsIgnoreCase(columnname.get(j)) != true) {
+					// System.out.println(cname[i]);
+					// System.out.print(true);
+					ResultSet rs1 = stat
+							.executeQuery("ALTER TABLE " + tabname + " MODIFY " + cname[i] + " " + ctype[i] + " NULL");
+				} else {
+					// System.out.println(cname[i] + " " + columnname.get(j));
+					j++;
+				}
+				// System.out.println(md.getColumnName(i + 1));
 			}
 
 		} catch (Exception e) {
@@ -71,4 +80,56 @@ public class App {
 		}
 	}
 
+	public static List<CSVRecord> getRecords() throws Exception {
+
+		File file = new File("C:/Users/LENOVO/Downloads/" + tabname + ".csv");
+		FileReader in = new FileReader(file);
+		CSVParser parse = CSVParser.parse(file, Charset.forName("UTF-8"), CSVFormat.RFC4180);
+		List<CSVRecord> records = parse.getRecords();
+		CSVRecord label = records.get(0);
+		Iterator<String> itr = label.iterator();
+
+		while (itr.hasNext()) {
+			columnname.add(itr.next());
+		}
+
+		return records;
+	}
+
+	public static void addData(Connection conn, PreparedStatement statement, ResultSet rs) {
+		try {
+			List<CSVRecord> records = getRecords();
+			int k = 0;
+			for (CSVRecord record : records) {
+				if (k == 0) {
+					k++;
+					continue;
+				}
+				rs.moveToInsertRow();
+				for (int i = 0, j = 0; i < n && j < n; i++) {
+					if (record.get(j).isEmpty()) {
+						continue;
+					}
+
+					if (cname[i].equalsIgnoreCase(columnname.get(j)) != true) {
+						;
+					} else {
+
+						System.out.println(record.get(j));
+						System.out.println(cname[i]);
+						rs.updateString(cname[i], record.get(j));
+						j++;
+					}
+
+				}
+				rs.insertRow();
+				rs.moveToCurrentRow();
+			}
+			conn.close();
+		} catch (SQLException ex) {
+			ex.printStackTrace();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
+	}
 }
